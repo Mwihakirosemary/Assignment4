750c4383b97366b007f3200b332023c7d236a5eb
Mwihakirosemary <kakirosemary@gmal.com>
2022-03-13 23:10:26 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main (){
+multi(arrayOf(15,20,4))
+    stateement(arrayOf("Laptop",20.5,15,17.2F,5,5.5,true))
+    createArray(arrayOf('a','e','i','o','u'))
+}
+fun multi(num: Array<Int>): Int {
+    var product = 1
+    num.forEach { mn ->
+        if (mn is Int) {
+            product *= mn
+        }
+    }
+    println(product)
+    return product
+}
+fun stateement(line: Array<Any>): Double {
+    var sum = 0.0
+    line.forEach { j ->
+        if (j is Double || j is Float) {
+            sum += j.toString().toDouble()
+        }
+    }
+    println(sum)
+    return sum
+}
+fun createArray(vowels: Array<Char>): Int{
+    var number = vowels.count()
+    println(number)
+    return number
+    }
+
